c3795c09b64eca5f455f1a1a52e717f867809087
ggjx22 <navlag18011@gmail.com>
2024-04-29 22:42:35 +0800

    basic set up of rag query engine with tools - improve parser output formatting through prompts - created tools to extract information about document type, customer and supplier details

--- src/user_query.py
+++ src/user_query.py
@@ -1,42 +1,88 @@
 def get_parser_instructions():
     return """
-    You are an intelligent document parser. You will be provided with either
-    an invoice or purchase order. Follow the instructions below for your
-    parsing tasks.
+    You are an intelligent document parser. You will only be provided with 
+    either an invoice or purchase order. Follow the instructions below step by 
+    step to parse the document and reconstruct it with key information 
+    formatted to the output formatting requirements given to you.
     
-    Step 1:
-    ALWAYS do this as the first step.
-    1. Identify the document type. If document is an invoice, look for keywords
-    in the document such as "invoice" or "tax invoice". If document is a
-    purchase order. Look for  "sales order" pr "purchase order".  
-    2. Identify the document sender and recipient. If document is an invoice,
-    the document sender is likely the supplier and document recipient is likely
-    the customer. If document is a purchase order, the document sender is
-    likely the customer and document recipient is likely the supplier.
+    STEP 1: Extract key information about the DOCUMENT TYPE
+    - Identify the document type as "invoice" or "purchase order".
+    - Look for keywords such as "tax invoice" or "invoice" for invoice.
+    - Look for keywords such as "sales order" or "purchase order" for purchase 
+    order.
+
+    STEP 2: Extract key information about the CUSTOMER
+    - Identify who is the customer in the document.
+    - If the document type is purchase order, the customer is likely the 
+    document sender.
+    - If document type is invoice, the customer is likely the document 
+    recipient.
+    - Provide all details about the customer such as the company name, address 
+    and contact details.
+    Notes: If you need more help to identify, look at the details about bank 
+    details and payment instructions in the document.
     
-    Step 2:
-    Try to extract the following key information from the document.
-    - Document type.
-    - Document country of origin.
-    - Document number.
-    - Document date.
-    - Reference document number.
-    - Details of document sender.
-    - Details of document recipient.
-    - Delivery location name.
-    - Delivery location address
-    - Invoicing address.
-    - Total amount excluding tax.
-    - Total discount amount.
-    - Total tax amount.
-    - Total amount including tax.
-    - Currency code used for transaction.
-    - Table containing itemized list of goods and services.
+    STEP 3: Extract key information about the SUPPLIER
+    - Identify who is the supplier in the document.
+    - If the document type is purchase order, the supplier is likely the 
+    document sender.
+    - If document type is invoice, the supplier is likely the document 
+    recipient.
+    - Provide all details about the supplier such as the company name, address 
+    and contact details.
+    Notes: If you need more help to identify, look at the details about bank 
+    details and payment instructions in the document. 
+    
+    STEP 4: Extract key information about the DOCUMENT ORIGIN
+    - Identify the country of origin of the document. The name of the country 
+    can be infer from the address of the document sender.
+    
+    STEP 5: Extract key information about the DOCUMENT NUMBER
+    - Identify the document number.
+    - If the document is a purchaser order, look for keywords like "purchase 
+    order number", "sales order number", "PO No.". 
+    - If the document is an invoicem look for keywords like "tax invoice 
+    number", "invoice number", "INV No.".
+    
+    STEP 6: Extract key information about the DELIVERY LOCATION NAME
+    - The name of the place at which the goods or services are to be delivered.
+    - If this information is not explicitly stated, no extraction required.
+    
+    STEP 7: Extract key information about the DELIVERY LOCATION ADDRESS
+    - The full address at which the goods or services are to be delivered.
+    - If this information not explicitly stated, no extraction required.
+    
+    STEP 8: Extract key information about the INVOICING ADDRESS
+    - The full billing address of the document.
+    - If document type is a purchase order, this will be the billing address 
+    of the customer.
+    - If document type is an invoice, this will be the billing address of the 
+    customer.
+    - If this information is not explicity stated, use the exact address of the 
+    customer.
+    
+    STEP 9: Extracy key information about the TOTAL AMOUNTS
+    - These amounts are Total Amount Excluding Tax, Tax Amount, Discount Amount 
+    and Total Amount Including Tax.
+    - When any of these information is not explicitly stated, DO NOT DO ANY 
+    CALCULATIONS TO DERIVE THE ANSWER.
+    
+    STEP 10: Extract key information about the CURRENCY CODE
+    - Identify the currency code used for the transaction between the customer 
+    and supplier in the document.
+    - If this information not explicitly stated, no extraction required.
+    
+    STEP 11: Extract key information about TABLE OF ITEMIZED LIST
+    -  Table of itemized list are goods or services provided by the supplier of 
+    the document.
+    - Parse the table row by row and column by column. DO NOT MISS OUT ANYTHING 
+    IN THE TABLE. DO NOT ALTER ANY DETAILS FOR THE TABLE.
     
     Output formatting requirements:
-    - Parse all key information and format the result with markdown section
-    headers and bullet points such that data extraction by an LLM is
-    straightforward.
-    - Parse all rows and columns of all tables in the document and nicely
-    formatting the result coherently using borders.  
+    - Parse all key information and format your response with markdown heading 
+    levels and key information in bullet points. No bold text formatting 
+    required.
+    - Parse all rows and columns of table of itemized list in the document 
+    nicely by formatting them with vertical line for columns and dashes for 
+    rows.
     """
